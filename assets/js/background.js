0c67f0e6d125e3f9189e3c1727ecb71afb0e4790
Pawan kumar <32352500+Pawan0411@users.noreply.github.com>
2018-10-12 01:50:17 +0530

    Initial commit

---
+++ assets/js/background.js
@@ -0,0 +1,21 @@
+
+// var colors = new Array(
+//     url('../images/wall2.png')
+// );
+var imgs = ["url('https://images2.imgbox.com/6b/2a/mOdIsvv5_o.png')",
+"url('https://images2.imgbox.com/19/97/1sxkAC9F_o.png')",
+"url('https://images2.imgbox.com/76/ef/bwUBlz4Z_o.png')",
+"url('https://images2.imgbox.com/32/4f/lN6kIo0V_o.png')"];
+var active = 0;
+setInterval(function(){
+    document.body.style.backgroundImage = imgs[active % imgs.length];
+    document.body.style.backgroundSize = 'cover';
+    document.body.style.backgroundAttachment = 'fixed';
+    active++;
+}, 2000);
+
+// https://images2.imgbox.com/6b/2a/mOdIsvv5_o.png
+// https://images2.imgbox.com/19/97/1sxkAC9F_o.png
+
+// https://images2.imgbox.com/76/ef/bwUBlz4Z_o.png
+// https://images2.imgbox.com/32/4f/lN6kIo0V_o.png
