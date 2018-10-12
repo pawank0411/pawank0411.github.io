9a60eda74ae130df2cf05e23fa019b35adc5fbdb
Md Azmal <azmalmd12@gmail.com>
2018-10-12 11:34:15 +0530

    fixed background transition

--- assets/js/background.js
+++ assets/js/background.js
@@ -3,16 +3,22 @@
 //     url('../images/wall2.png')
 // );
 var imgs = ["url('https://images2.imgbox.com/6b/2a/mOdIsvv5_o.png')",
-"url('https://images2.imgbox.com/19/97/1sxkAC9F_o.png')",
-"url('https://images2.imgbox.com/76/ef/bwUBlz4Z_o.png')",
-"url('https://images2.imgbox.com/32/4f/lN6kIo0V_o.png')"];
+    "url('https://images2.imgbox.com/19/97/1sxkAC9F_o.png')",
+    "url('https://images2.imgbox.com/76/ef/bwUBlz4Z_o.png')",
+    "url('https://images2.imgbox.com/32/4f/lN6kIo0V_o.png')"];
 var active = 0;
-setInterval(function(){
+
+changeBg();
+
+function changeBg() {
     document.body.style.backgroundImage = imgs[active % imgs.length];
     document.body.style.backgroundSize = 'cover';
     document.body.style.backgroundAttachment = 'fixed';
+    document.body.style.transition = "background 1s";
     active++;
-}, 2000);
+}
+
+setInterval(changeBg, 2000);
 
 // https://images2.imgbox.com/6b/2a/mOdIsvv5_o.png
 // https://images2.imgbox.com/19/97/1sxkAC9F_o.png
